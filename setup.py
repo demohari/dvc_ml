71bcd51d05d87f332154453affb30f831de0ff3c
demohari <kishore_koneru@yahoo.com>
2022-11-24 23:55:43 +0000

    stage 01 created

--- setup.py
+++ setup.py
@@ -4,19 +4,15 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="src",
-    version="0.0.1",
-    author="Hari",
-    description="A small package for dvc ml pipeline demo",
+    name="src",  # How you named your package folder (MyLib)
+    version="0.0.1",  # Start with a small number and increase it with every change you make
+    author="Hari",  # Type in your name
+    description="A small package for dvc ml pipeline demo",  # Give a short description about your library
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/demohari/dvc_ml",
-    author_email="kishore_koneru@yahoo.com",
+    url="https://github.com/demohari/dvc_ml",  # Provide either the link to your github or to your website
+    author_email="kishore_koneru@yahoo.com",  # Type in your E-Mail
     packages=["src"],
-    python_requires=">=3.7",
-    install_requires=[
-        'dvc',
-        'pandas',
-        'scikit-learn'
-    ]
+    python_requires=">=3.7",  # Specify which pyhton versions that you want to support
+    install_requires=["dvc", "pandas", "scikit-learn"],  # requirements
 )
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -2,13 +2,11 @@ import yaml
 import os
 import json
 
-
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
 
     return content
-
 
 def create_directory(dirs: list):
     for dir_path in dirs:
