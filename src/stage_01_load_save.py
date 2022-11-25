bfc54bc9ab7715210bbb806f61aa72357e809fca
demohari <kishore_koneru@yahoo.com>
2022-11-25 00:49:20 +0000

    update

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -1,14 +1,14 @@
-from src.utils.all_utils import read_yaml, create_directory
+import os
 import argparse
 import pandas as pd
-import os
+from src.utils.all_utils import read_yaml, create_directory
 
 
 def get_data(config_path):
     config = read_yaml(config_path)
     remote_data_path = config["data_source"]
     df = pd.read_csv(remote_data_path, sep=";")
-    #print(df.head())
+    # print(df.head())
 
     # save dataset in the local directory
     # create path to directory: artifacts/raw_local_dir/data.csv
@@ -23,7 +23,7 @@ def get_data(config_path):
     raw_local_file_path = os.path.join(raw_local_dir_path, raw_local_file)
 
     df.to_csv(raw_local_file_path, sep=",", index=False)
-    
+
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
