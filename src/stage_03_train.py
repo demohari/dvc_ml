ea129b919b216a77477026e276c41d0a4997a62d
demohari <kishore_koneru@yahoo.com>
2022-11-25 22:35:01 +0000

    evaluate stage added

--- src/stage_03_train.py
+++ src/stage_03_train.py
@@ -29,14 +29,14 @@ def train(config_path, params_path):
 
     lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
     lr.fit(train_x, train_y)
-    
-    model_dir=config["artifacts"]["model_dir"]
-    model_filename = config["artifacts"]["model_file"]
-    model_dir = os.path.join(artifacts_dir,model_dir)
+
+    model_dir = config["artifacts"]["model_dir"]
+    model_filename = config["artifacts"]["model_filename"]
+    model_dir = os.path.join(artifacts_dir, model_dir)
 
     create_directory([model_dir])
     model_path = os.path.join(model_dir, model_filename)
-    joblib.dump(lr,model_path)
+    joblib.dump(lr, model_path)
 
 
 if __name__ == "__main__":
